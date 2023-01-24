1b79e1b8022bd1fc7b723c1242e082845eaeb79b
Adrianna <chongadrianna@gmail.com>
2023-01-24 19:11:24 +0800

    token wip

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('users/', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 
 ]
